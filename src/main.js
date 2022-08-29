0f5a5cfa12ce7f31a094abc0d1d59ece06f6d5f7
Asgarov Asgar <asgara@maestro.st>
2022-08-29 23:56:28 +0400

    initialized app

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import store from './store'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import defaultBoard from './defaultBoard'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    board: defaultBoard
+  },
+  mutations: {}
+})
